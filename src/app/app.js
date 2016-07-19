2688ef262e674d4b5064d21b49e7d361b3afd6df
Keeley Madigan <johnanthony.dev@gmail.com>
2016-07-18 21:26:50 -0700

    Made revisions

--- src/app/app.js
+++ src/app/app.js
@@ -1,2 +1,7 @@
 angular
-  .module('app', [] );
+  .module('app', ['satellizer'] )
+  .config(function($authProvider){
+    $authProvider.github({
+      clientId: '0b3f9d81cd061c6f5f97'
+    });
+  });
